6ac55fcd798d89e2d0ffa7292c4be16c932c67f7
ryfe <denis.koptelev@gmail.com>
2024-03-30 21:12:06 +0300

    fix urls. add new static

--- catalog/urls.py
+++ catalog/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('catalog/product_edit/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
     path('catalog/product_delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete')
 
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                               document_root=settings.STATIC_ROOT)
